e73a0b788c00eabe1bb53849a40ce5a10eab7de7
hanouw <jehasite@gmail.com>
2023-11-11 13:17:41 +0900

    jeha

--- call_xlsx.py
+++ call_xlsx.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import os
+os.system('cls')
 
 # 엑셀 파일 불러오기
 excel_file_path = 'report.xlsx'
@@ -15,7 +17,7 @@ df_filtered_과목종별_GLC교양 = df[(~df['평가'].isin(['W', 'NP', 'F', 'U'
 
 
 # Define required credits for each category
-required_credits = {"전공기초": 20, "전공필수": 30, "전공선택": 15, "일반교양": 20, "교양기초": 10, "대학교양": 5, "공통기초": 10}
+required_credits = {"전공기초": 20, "전공필수": 30, "전공선택": 15, "RC": 2, "GLC교양": 10, "대학교양": 5, "공통기초": 10}
 
 # Calculate the sum of completed credits in each category
 completed_credits_전기 = df_filtered_과목종별_전기.groupby("학점").sum()
@@ -24,17 +26,20 @@ completed_credits_전필 = df_filtered_과목종별_전필.groupby("학점").sum
 completed_credits_RC = df_filtered_과목종별_RC.groupby("학점").sum()
 completed_credits_GLC교양 = df_filtered_과목종별_GLC교양.groupby("학점").sum()
 
-remaining_credits = {
-    "전기": required_credits["전기"] - completed_credits_전기,
-    "전선": required_credits["전선"] - completed_credits_전선,
-    "전필": required_credits["전필"] - completed_credits_전필,
-    "RC": required_credits["RC"] - completed_credits_RC,
-    "GLC교양": required_credits["GLC교양"] - completed_credits_GLC교양
-}
+print(required_credits["전공기초"])
+print
 
-# Create a DataFrame for the output
-output_df = pd.DataFrame(list(remaining_credits.items()), columns=["Category", "Remaining Credits"])
+# remaining_credits = {
+#     "전기": required_credits["전공기초"] - completed_credits_전기,
+#     "전선": required_credits["전공선택"] - completed_credits_전선,
+#     "전필": required_credits["전공필수"] - completed_credits_전필,
+#     "RC": required_credits["RC"] - completed_credits_RC,
+#     "GLC교양": required_credits["GLC교양"] - completed_credits_GLC교양
+# }
 
-# Write to an Excel file
-output_df.to_excel("result_file.xlsx", index=False)
+# # Create a DataFrame for the output
+# output_df = pd.DataFrame(list(remaining_credits.items()), columns=["Category", "Remaining Credits"])
+
+# # Write to an Excel file
+# output_df.to_excel("result_file.xlsx", index=False)
 
