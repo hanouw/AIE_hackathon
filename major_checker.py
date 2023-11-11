a9ebdb1e62d96d4a4f7aab228dc1c648f3314ec2
hanouw <jehasite@gmail.com>
2023-11-11 23:44:37 +0900

    jeha

--- major_checker.py
+++ major_checker.py
@@ -11,32 +11,19 @@ def read_major():
 
 
 def get_additional_majors():
-    additional_programs_count = input("Enter the number of 복수전공, 부전공, 심화전공 you have, separated by space (e.g., 1 0 1 for one 복수전공 and one 심화전공): ")
-    counts = additional_programs_count.split()
+    major_num, minor_num, advanced_num = map(int, input("Enter the number of 복수전공, 부전공, 심화전공 you have, separated by space (e.g., 1 0 1 for one 복수전공 and one 심화전공): ").split())
+    major_list=[]
+    minor_list=[]
+    advanced_list=[]
 
-    majors_dict = {"복수전공": [], "부전공": [], "심화전공": []}
-    if len(counts) >= 1 and int(counts[0]) > 0:
-        majors_dict["복수전공"].extend([input(f"Enter 복수전공 {i+1}: ").strip() for i in range(int(counts[0]))])
-    if len(counts) >= 2 and int(counts[1]) > 0:
-        majors_dict["부전공"].extend([input(f"Enter 부전공 {i+1}: ").strip() for i in range(int(counts[1]))])
-    if len(counts) >= 3 and int(counts[2]) > 0:
-        majors_dict["심화전공"].extend([input(f"Enter 심화전공 {i+1}: ").strip() for i in range(int(counts[2]))])
+    for a in range (major_num):
+        major_list.append(input())
 
-    double_majors = majors_dict["복수전공"]
-    minors = majors_dict["부전공"]
-    advanced_majors = majors_dict["심화전공"]
-    return majors_dict, double_majors, minors, advanced_majors
+    for b in range (minor_num):
+        minor_list.append(input())
 
-if __name__ == "__main__":
-    # Example usage
-    primary_major = read_major()
-    additional_majors_dict = get_additional_majors()
+    for c in range (advanced_num):
+        advanced_list.append(input())
 
-    # Accessing majors by type
-    double_majors = additional_majors_dict["복수전공"]
-    minors = additional_majors_dict["부전공"]
-    advanced_majors = additional_majors_dict["심화전공"]
+    return major_num, minor_num, advanced_num, major_list, minor_list, advanced_list
 
-    print(double_majors)
-    print(minors)
-    print(advanced_majors)
