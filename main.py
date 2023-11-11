73f44872c71f1837418be3684f1361d3fdc56aff
junghyun <mattew423@gmail.com>
2023-11-11 23:22:24 +0900

    simplfying

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from major_checker import read_major, get_additional_majors
 from SingleMajor import SingleMajor
 
 if __name__ == "__main__":
-    SingleMajor()
+    main_major = read_major()
+    print(main_major)
+    get_additional_majors()
+
+
+if __name__ == "__main__":
+    analyze_major()
     
