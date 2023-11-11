793b1243f8f35bf03542820de2d78ff67d163607
junghyun <mattew423@gmail.com>
2023-11-12 00:25:24 +0900

    dddd

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from major_checker import read_major, get_additional_majors
-from SingleMajor import SingleMajor
+from SingleMajor import single_major
 from MultiMajor import multi_majors
 
 if __name__ == "__main__":
     main_major = read_major()
     print(main_major)
     get_additional_majors()
-    multi_majors()
+    multi_majors(main_major)
+    single_major(main_major)
 
 
 
