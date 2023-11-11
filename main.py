00a0cf666d3b7aa53c006d17c31aded76d790be5
hanouw <jehasite@gmail.com>
2023-11-11 15:04:11 +0900

    jeha

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 import pandas as pd
+from read_major import read_major
 
-# Load the Excel file
-df = pd.read_excel("path_to_file.xlsx")
-
-# Define required credits for each category
-required_credits = {"전공기초": 20, "전공필수": 30, "전공선택": 15, "일반교양": 20, "교양기초": 10, "대학교양": 5, "공통기초": 10}
-
-# Calculate the sum of completed credits in each category
-completed_credits = df.groupby("Category")["Credits"].sum()
-
-# Calculate remaining credits
-remaining_credits = {category: required_credits[category] - completed_credits.get(category, 0) for category in required_credits}
-
-# Create a DataFrame for the output
-output_df = pd.DataFrame(list(remaining_credits.items()), columns=["Category", "Remaining Credits"])
-
-# Write to an Excel file
-output_df.to_excel("remaining_credits.xlsx", index=False)
+if __name__ == "__main__":
+    first_major = read_major()
+    print(first_major)
+    
