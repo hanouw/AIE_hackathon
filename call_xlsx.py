cb1a2389d4603babb1fae53083f9bd87a099268f
hanouw <jehasite@gmail.com>
2023-11-11 12:36:23 +0900

    jeha

--- call_xlsx.py
+++ call_xlsx.py
@@ -5,19 +5,18 @@ excel_file_path = 'report.xlsx'
 df = pd.read_excel(excel_file_path)
 
 # Filter out courses with 'W' grade
-df_filtered_평가 = df[df['평가'] != 'W' and df['평가'] != 'NP' and df['평가'] != 'F']
-df_filtered_과목종별_전기 = df[df['과목종별'] == '전기']
-df_filtered_과목종별_전선 = df[df['과목종별'] == '전선']
-df_filtered_과목종별_전필 = df[df['과목종별'] == '전필']
-df_filtered_과목종별_RC = df[df['과목종별'] == 'RC']
-df_filtered_과목종별_GLC교양 = df[df['과목종별'] == '대교' and df['학정번호'].str[:3] == 'GLC']
-# filtered_df = df[df['학정번호'].str[:3] == 'GLC']
+df_filtered_과목종별_전기 = df[~df['평가'].isin(['W', 'NP', 'F', 'U']) & (df['과목종별'] == '전기')]
+df_filtered_과목종별_전선 = df[~df['평가'].isin(['W', 'NP', 'F', 'U']) & (df['과목종별'] == '전선')]
+df_filtered_과목종별_전필 = df[~df['평가'].isin(['W', 'NP', 'F', 'U']) & (df['과목종별'] == '전필')]
+df_filtered_과목종별_RC = df[~df['평가'].isin(['W', 'NP', 'F', 'U']) & (df['과목종별'] == 'RC')]
+df_filtered_과목종별_GLC교양 = df[~df['평가'].isin(['W', 'NP', 'F', 'U']) & df['과목종별'] == '대교' and df['학정번호'].str[:3] == 'GLC']
+
 
 # Define required credits for each category
 required_credits = {"전공기초": 20, "전공필수": 30, "전공선택": 15, "일반교양": 20, "교양기초": 10, "대학교양": 5, "공통기초": 10}
 
 # Calculate the sum of completed credits in each category
-completed_credits = df_filtered.groupby("Category")["Credits"].sum()
+completed_credits = df_filtered_과목종별_전기.groupby("학점").sum()
 
 # Calculate remaining credits
 remaining_credits = {category: required_credits[category] - completed_credits.get(category, 0) for category in required_credits}
