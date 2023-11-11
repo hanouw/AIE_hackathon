24028b4c1862650f8d123338ccc7618bf2d98b2a
junghyun <mattew423@gmail.com>
2023-11-12 07:06:10 +0900

    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     main_major = read_major()
     # print(main_major)
     major_list, minor_list, advanced_list= get_additional_majors()
-    if len(major_list != 0):
+    if (len(major_list) != 0):
         multi_majors(main_major,major_list, minor_list, advanced_list)
     else:
         single_major(main_major, minor_list, advanced_list)
