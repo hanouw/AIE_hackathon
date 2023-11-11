b57fba7c7fdefeb898cfeb81127a9cea723e1909
junghyun <mattew423@gmail.com>
2023-11-11 13:21:34 +0900

    gg

--- junghyun.py
+++ junghyun.py
@@ -14,30 +14,44 @@ class Major:
     def calculate_remaining_credits(self):
         return {category: self.required_credits[category] - self.completed_credits for category in self.required_credits}
 
+def extract_major(소속):
+    if '대학' in 소속 and '전공' in 소속:
+        start = 소속.find('대학') + len('대학')
+        end = 소속.find('전공')
+        return 소속[start:end].strip()
+    return None
+
 # Load the Excel file
 excel_file_path = 'report.xlsx'
-df = pd.read_excel(excel_file_path, header=3)  # Adjust the header row as needed
+df = pd.read_excel(excel_file_path)
 
-# Define required credits for each category (common for all majors)
-required_credits = {"전공기초": 20, "전공필수": 30, "전공선택": 15, "일반교양": 20, "교양기초": 10, "대학교양": 5, "공통기초": 10}
+# Extract the student's major
+student_major_info = df.iloc[1]['소속']  # Assuming the major is in the second row
+student_major = extract_major(student_major_info)
+
+# Course data starts from the third row onward
+course_data = df.iloc[2:]
+
+# Define unique required credits for each major
+# ...
 
 # Define the unique course codes for each major
-majors = [
-    Major("응용정보공학", ["APL101", "APL102", ...], required_credits),  # Replace with actual course codes
-    Major("바이오생명공학", ["BIO201", "BIO202", ...], required_credits),  # Replace with actual course codes
-    # Add other majors similarly
-]
+# ...
 
-# Calculate completed and remaining credits for each major
-for major in majors:
-    major.calculate_completed_credits(df)
-    remaining_credits = major.calculate_remaining_credits()
+# Find the Major object for the student's major
+major_obj = majors_dict.get(student_major)
+if not major_obj:
+    raise ValueError("Major not found in the defined majors")
 
-    # Create a DataFrame for the output
-    output_df = pd.DataFrame(list(remaining_credits.items()), columns=["Category", "Remaining Credits"])
+# Calculate completed and remaining credits for the student's major
+major_obj.calculate_completed_credits(course_data)
+remaining_credits = major_obj.calculate_remaining_credits()
 
-    # Write to an Excel file
-    output_file_name = f"{major.name}_result_file.xlsx"
-    output_df.to_excel(output_file_name, index=False)
+# Create a DataFrame for the output
+output_df = pd.DataFrame(list(remaining_credits.items()), columns=["Category", "Remaining Credits"])
 
-    print(f"Results for {major.name} written to {output_file_name}")
+# Write to an Excel file
+output_file_name = f"{major_obj.name}_result_file.xlsx"
+output_df.to_excel(output_file_name, index=False)
+
+print(f"Results for {major_obj.name} written to {output_file_name}")
